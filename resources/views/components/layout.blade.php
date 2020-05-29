0fb51dbbaabf72f060b7c97c9bdeb8b24e7f2633
Jeffrey Way <jeffrey@jeffrey-way.com>
2020-05-29 13:11:52 -0400

    Episode 1 Complete

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1"
+    >
+
+    <title>Laravel</title>
+
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+          rel="stylesheet"
+    >
+</head>
+
+<body>
+
+    {{ $slot }}
+
+</body>
+
+</html>
---
+++ resources/views/components/section.blade.php
@@ -0,0 +1,5 @@
+<section class="bg-blue-200 p-4 mb-6">
+    <div class="container">
+       {{ $slot }}
+    </div>
+</section>
