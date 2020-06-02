b24567c633916627ea8a1f125ed350dc5c1e03c0
Jeffrey Way <jeffrey@jeffrey-way.com>
2020-06-02 13:19:49 -0400

    Episode 3 complete

---
+++ resources/views/comments/edit.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+    <x-section>
+        <x-form
+            method="PATCH"
+            action="/comments/{{ $comment->id }}"
+        >
+            <div class="mb-6">
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                       for="body"
+                >
+                    Body
+                </label>
+
+                <textarea class="border border-gray-400 p-2 w-full"
+                          name="body"
+                          id="body"
+                          required
+                >{{ $comment->body }}</textarea>
+
+                @error('body')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <button type="submit"
+                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
+                >
+                    Submit
+                </button>
+            </div>
+        </x-form>
+    </x-section>
+</x-layout>
---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'method' => 'POST',
+    'action' => ''
+])
+
+<form method="{{ $method === 'GET' ? 'GET' : 'POST' }}"
+    action="{{ $action }}" {{ $attributes }}
+>
+    @csrf
+
+    @if (! in_array($method, ['GET', 'POST']))
+        @method($method)
+    @endif
+
+    {{ $slot }}
+</form>
