58c735e6a2e296d08f6639b22c65364e43879126
Jeffrey Way <jeffrey@jeffrey-way.com>
2020-06-04 15:20:17 -0400

    Modal episode complete

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,8 @@
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
           rel="stylesheet"
     >
+
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
-    <h1>Hello World</h1>
+    <x-modal
+        title="Deactivate Your Account?"
+        submit-label="Deactivate"
+    >
+        <x-slot name="trigger">
+            <button @click="on = true">Show Modal</button>
+        </x-slot>
+
+        Are you really sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+    </x-modal>
 </x-layout>
